3d31d7147b12caa28b63f4e0d1a096be169c6a36
Angad Kumar <angadkumar16ak@gmail.com>
2025-08-12 10:05:04 -0500

    Utils

--- ANCHOR/Utils/Color+Theme.swift
+++ ANCHOR/Utils/Color+Theme.swift
@@ -5,3 +5,9 @@
 //  Created by Angad Kumar on 8/12/25.
 //
 
+import SwiftUI
+
+extension Color {
+    static let anchorPrimary = Color("AnchorPrimary") // define in Assets
+    static let anchorAccent = Color("AnchorAccent")
+}
--- ANCHOR/Utils/DateFormatter+Extensions.swift
+++ ANCHOR/Utils/DateFormatter+Extensions.swift
@@ -5,3 +5,25 @@
 //  Created by Angad Kumar on 8/12/25.
 //
 
+import Foundation
+
+extension Date {
+    func shortDateString() -> String {
+        let df = DateFormatter()
+        df.dateStyle = .medium
+        df.timeStyle = .none
+        return df.string(from: self)
+    }
+}
+extension DateFormatter {
+    static let shared: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    func shortDateString(from date: Date) -> String {
+        return self.string(from: date)
+    }
+}
