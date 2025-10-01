7b0cc9ead7dc60e74893fa686dfae1c3f22bdce0
Angad Kumar <angadkumar16ak@gmail.com>
2025-10-01 17:11:26 -0500

    Auto: placeholder feature ExportCSV1759356686

---
+++ Tests/Features/ExportCSV1759356686Tests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Anchor
+
+final class ExportCSV1759356686Tests: XCTestCase {
+    func testScaffold() {
+        let vm = ExportCSV1759356686ViewModel()
+        XCTAssertNotNil(vm)
+    }
+}
