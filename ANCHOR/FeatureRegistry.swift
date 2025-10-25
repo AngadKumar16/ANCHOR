44ba6e64aaab9c2a9dcb5017ef1b034064e60983
Angad Kumar <angadkumar16ak@gmail.com>
2025-10-25 13:39:14 -0500

    Feature Registry

---
+++ ANCHOR/FeatureRegistry.swift
@@ -0,0 +1,17 @@
+// Auto-generated FeatureRegistry to ensure views are referenced
+import SwiftUI
+import ANCHORFeatures_Dashboard
+
+func __registerFeaturesForLinking() {
+    _ = AIAnalysisServiceView()
+    _ = CloudSyncSettings1759356720View()
+    _ = ConflictResolutionViewModelView()
+    _ = Dashboard.DashboardViewView()
+    _ = EmailpasswordLoginViewModelView()
+    _ = EntryCategorizationtaggingViewModelView()
+    _ = ImplementBackuprestoreFunctionalityViewModelView()
+    _ = ImplementUserAuthenticationServiceViewModelView()
+    _ = InitialSyncImplementationViewModelView()
+    _ = JournalEntryViewView()
+    _ = MoodTrackingInterfaceViewModelView()
+}
